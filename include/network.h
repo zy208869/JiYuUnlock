9fef0da61d0759c4a05f5ac819f3c4db89a828ef
zy208869 <3175469250@qq.com>
2026-06-24 10:02:33 +0800

    更新 network.h

--- include/network.h
+++ include/network.h
@@ -1,1 +1,20 @@
 
+#pragma once
+
+#include <winsock2.h>
+#include <windows.h>
+#include <string>
+
+class NetworkManager {
+public:
+    NetworkManager();
+    ~NetworkManager();
+
+    bool Init();
+    bool SendCancelBlackScreen();
+
+private:
+    SOCKET m_udpSocket;
+    sockaddr_in m_broadcastAddr;
+    bool m_isInitialized;
+};
