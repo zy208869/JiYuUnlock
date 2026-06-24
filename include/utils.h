06e7352d682ae257a348457a6642ce193dcdc5a5
zy208869 <3175469250@qq.com>
2026-06-24 10:04:09 +0800

    更新 utils.h

--- include/utils.h
+++ include/utils.h
@@ -1,8 +1,4 @@
 #pragma once
-#include <string>
 #include <windows.h>
 
-DWORD GetProcessIdByName(const std::string& processName);
-bool KillProcessByName(const std::string& processName);
-bool SuspendProcessByName(const std::string& processName);
-bool ResumeProcessByName(const std::string& processName);
+bool EnableDebugPrivilege();
